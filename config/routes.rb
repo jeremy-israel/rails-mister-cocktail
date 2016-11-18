2f072191df5375d34ed0d6b7d721e56bac499940
Jeremy ISRAEL <israel.jeremy@gmail.com>
2016-11-18 23:20:19 +0100

    modifs

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :destroy]
   end
   # resources :doses, only: :destroy
+  root to: "cocktails#index"
   mount Attachinary::Engine => "/attachinary"
 end
